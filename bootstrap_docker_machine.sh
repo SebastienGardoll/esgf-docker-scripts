799dfc71d367a37e47046c7c1ed8b01b60b5e84b
Sébastien Gardoll <sebastien.gardoll@ipsl.fr>
2017-10-06 11:57:27 +0200

    add bootstrap for docker machine

---
+++ bootstrap_docker_machine.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+################################# SETTINGS #####################################
+
+readonly BASE_DIR_PATH="$(pwd)"
+SCRIPT_PARENT_DIR_PATH="$(dirname $0)"; cd "${SCRIPT_PARENT_DIR_PATH}"
+readonly SCRIPT_PARENT_DIR_PATH="$(pwd)" ; cd "${BASE_DIR_PATH}"
+
+set -u
+
+source "${SCRIPT_PARENT_DIR_PATH}/set_env"
+
+readonly ARCHIVE_FILENAME='config.tar.xz'
+readonly ARCHIVE_FILE_PATH="${SCRIPT_PARENT_DIR_PATH}/${ARCHIVE_FILENAME}"
+
+############################ CONTROL VARIABLES #################################
+
+is_standalone="${1-false}"
+
+################################ FUNCTIONS #####################################
+
+function startup_cluster {
+
+rm -f "${ARCHIVE_FILE_PATH}"
+
+pushd "${DOCKER_HOME}" > /dev/null
+
+echo "> create the configuration archive"
+tar --owner=0 --group=0 -pcJvf "${ARCHIVE_FILE_PATH}" "${ESGF_CONFIG_DIRNAME}"
+
+echo "> copy the configuration"
+docker-machine ssh node0 "mkdir -p ${DOCKER_HOME} ; chmod go= ${DOCKER_HOME}"
+docker-machine scp "${ARCHIVE_FILE_PATH}" node0:${DOCKER_HOME}
+echo "> extract the configuration"
+docker-machine ssh node0 "tar -xJvf ${DOCKER_HOME}/${ARCHIVE_FILENAME} -C ${DOCKER_HOME}"
+
+echo "> copy the configuration"
+docker-machine ssh node1 "mkdir -p ${DOCKER_HOME} ; chmod go= ${DOCKER_HOME}"
+docker-machine scp "${ARCHIVE_FILE_PATH}" node1:${DOCKER_HOME}
+echo "> extract the configuration"
+docker-machine ssh node1 "tar -xJvf ${DOCKER_HOME}/${ARCHIVE_FILENAME} -C ${DOCKER_HOME}"
+
+rm "${ARCHIVE_FILE_PATH}"
+
+popd > /dev/null
+
+echo "> update labels"
+
+docker-machine ssh node0 "docker node update --label-add esgf_front_node=true node0"
+docker-machine ssh node0 "docker node update --label-add esgf_idp_node=true node0"
+docker-machine ssh node0 "docker node update --label-add esgf_index_node=true node0"
+docker-machine ssh node0 "docker node update --label-add esgf_solr_node=true node0"
+docker-machine ssh node0 "docker node update --label-add esgf_db_node=true node1"
+docker-machine ssh node0 "docker node update --label-add esgf_data_node=true node1"
+}
+
+
+################################## MAIN ########################################
+
+systemctl --no-pager status docker > /dev/null
+
+if [ $? -ne 0 ]; then
+  echo "> starting docker"
+  sudo service docker start
+fi
+
+echo "> init configuration"
+"${DOCKER_GIT_DIR_PATH}/scripts/esgf_node_init.sh"
+chmod go= "${ESGF_CONFIG}"
+
+if [[ "${is_standalone}" = "${TRUE}" ]]; then
+  startup_standalone
+else
+  startup_cluster
+fi
+
+eval $(docker-machine env node0)
+
+docker stack deploy -c "${DOCKER_GIT_DIR_PATH}/docker-stack.yml" esgf-stack
+
+watch -n 1 docker service ls
